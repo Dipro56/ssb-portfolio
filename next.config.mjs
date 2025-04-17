c00cada0f100ac7df0f18e862ede4efb3bc8dcf0
Sadat Shahriar <sadat@gakkmediabd.com>
2025-04-17 13:17:28 +0600

    config update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  distDir: "out",
+  images: {
+    unoptimized: true, // Disable image optimization
+  },
+};
 
 export default nextConfig;
